acfec28c9802b04ec90c9e1d935236e1476adf96
rockethelll <rockethell@hotmail.com>
2025-04-07 18:10:29 +0200

    add license

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 export default function Home() {
-  return (<div>
-    <div className='text-preset-1'>test</div>
-    <div className='text-preset-2'>test</div>
-    <div className='text-preset-3'>test</div>
-    <div className='text-preset-4'>test</div>
-    <div className='text-preset-4-bold'>test</div>
-    <div className='text-preset-5'>test</div>
-    <div className='text-preset-5-bold'>test</div>
-    
-  </div>);
+  return (
+    <div>
+      <div className='text-preset-1'>test</div>
+      <div className='text-preset-2'>test</div>
+      <div className='text-preset-3'>test</div>
+      <div className='text-preset-4'>test</div>
+      <div className='text-preset-4-bold'>test</div>
+      <div className='text-preset-5'>test</div>
+      <div className='text-preset-5-bold'>test</div>
+    </div>
+  );
 }
