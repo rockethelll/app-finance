f0c5c12f002334fcad651a45ad487eba16f0ce41
rockethelll <rockethell@hotmail.com>
2025-04-09 17:28:46 +0200

    add google provider for login and add middleware

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,24 +1,25 @@
-'use client';
-
 import { SignoutButton } from '@/components/Signout-button';
-import { authClient } from '@/lib/auth-client';
+import { getUser } from '@/lib/auth-session';
+import Link from 'next/link';
 
-export default function DashboardPage() {
-  const { data: session } = authClient.useSession();
+export default async function DashboardPage() {
+  const user = await getUser();
 
   return (
-    <div>
-      {session?.user?.email}
+    <div className='p-16'>
+      {user?.email}
       <br />
-      {session?.user?.name}
+      {user?.name}
       <br />
-      {session?.user?.image && (
-        <img src={session?.user?.image} alt="user image" />
-      )}
+      {user?.image && <img src={user?.image} alt="user image" />}
       <br />
-      {session?.user?.emailVerified}
+      {user?.emailVerified}
       <br />
       <SignoutButton />
+      <div className="flex mt-8 gap-4">
+        <Link href="/login">Login</Link>
+        <Link href="/signup ">Signup</Link>
+      </div>
     </div>
   );
 }
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -16,10 +16,10 @@ export const auth = betterAuth({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
     },
-  //   google: {
-  //     clientId: process.env.GOOGLE_CLIENT_ID!,
-  //     clientSecret: process.env.GOOGLE_CLIENT_SECRET!
-  // },
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!
+  },
   },
   plugins: [
     // Always in the last position
