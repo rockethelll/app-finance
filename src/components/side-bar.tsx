49737bc9086fa6f164a9cbeec9e628c972559da4
rockethelll <rockethell@hotmail.com>
2025-04-11 16:35:44 +0200

    Sidebar ok and responsive, add data to mock, move jpg in an other file data

--- src/components/side-bar.tsx
+++ src/components/side-bar.tsx
@@ -3,23 +3,86 @@
 import Tabs from '@/components/tabs';
 import { navIcons } from '@/lib/constants';
 import { usePathname } from 'next/navigation';
+import Image from 'next/image';
+import largeLogo from '../../public/images/logo-large.svg';
+import smallLogo from '../../public/images/logo-small.svg';
+import { ArrowBigLeftDash, ArrowBigRightDash } from 'lucide-react';
+import { cn } from '@/lib/utils';
+import useSidebarStore from '@/stores/sidebar-store';
+import { useEffect } from 'react';
 
 const SideBar = () => {
   const pathname = usePathname();
+  const { isCollapsed, toggleSidebar, isLoading, setLoaded } =
+    useSidebarStore();
   const isActive = (href: string) => pathname === href;
 
+  // This is to prevent the sidebar from flashing when the page is loaded
+  useEffect(() => {
+    setLoaded();
+  }, [setLoaded]);
+
+  if (isLoading) {
+    return null;
+  }
+
   return (
-    <nav className="absolute bg-grey-900 rounded-t-xl lg:rounded-l-none lg:rounded-r-xl h-[52px] md:h-[74px] bottom-0 w-full lg:top-0 lg:w-[300px] lg:h-screen flex justify-evenly items-end">
+    <nav
+      className={cn(
+        'absolute bg-grey-900 rounded-t-xl lg:rounded-l-none lg:rounded-r-xl h-[52px] md:h-[74px] bottom-0 w-full lg:top-0 lg:h-screen flex lg:flex-col justify-evenly lg:justify-start items-end lg:items-start lg:space-y-1 transition-all duration-300',
+        isCollapsed ? 'lg:w-[100px]' : 'lg:w-[300px]',
+      )}
+    >
+      {isCollapsed ? (
+        <Image
+          src={smallLogo}
+          alt="Small logo"
+          height={30}
+          width={50}
+          className="hidden h-32 pl-8 mb-6 text-center transition-opacity duration-300 lg:inline"
+        />
+      ) : (
+        <Image
+          src={largeLogo}
+          alt="Large logo"
+          fill={false}
+          height={30}
+          width={200}
+          className="hidden h-32 pl-8 mb-6 text-center transition-opacity duration-300 lg:inline"
+        />
+      )}
       {navIcons.map((icon) => (
         <Tabs
           key={icon.href}
-          icon={icon.icon}
-          href={icon.href}
-          label={icon.label}
-          isActive={isActive(icon.href)}
+          tab={{
+            icon: icon.icon,
+            href: icon.href,
+            label: isCollapsed ? '' : icon.label,
+            isActive: isActive(icon.href),
+          }}
+          isCollapsed={isCollapsed}
         />
-
       ))}
+      <div
+        onClick={toggleSidebar}
+        className={cn(
+          'h-[56px] w-[276px] rounded-r-lg bg-grey-900 hidden lg:flex items-center pl-8 mt-auto mb-32 cursor-pointer group',
+          isCollapsed && 'w-[90px] justify-center pl-0',
+        )}
+      >
+        <div className="flex items-center space-x-4">
+          {isCollapsed ? (
+            <ArrowBigRightDash className="w-6 h-6 transition-all duration-300 text-grey-300 group-hover:text-beige-100" />
+          ) : (
+            <>
+              <ArrowBigLeftDash className="w-6 h-6 transition-all duration-300 text-grey-300 group-hover:text-beige-100" />
+              <p className="transition-all duration-300 custom-text-preset-5-bold lg:text-base lg:font-bold text-grey-300 group-hover:text-beige-100">
+                Minimize Menu
+              </p>
+            </>
+          )}
+        </div>
+      </div>
     </nav>
   );
 };
--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -3,23 +3,34 @@
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
 
-type TagsProps = {
+type TabItem = {
   icon: React.ReactNode;
   href: string;
   label: string;
   isActive: boolean;
 };
 
-const Tabs = ({ icon, href, label, isActive }: TagsProps) => {
+type TabsProps = {
+  tab: TabItem;
+  isCollapsed: boolean;
+};
+
+const Tabs = ({
+  tab: { icon, href, label, isActive },
+  isCollapsed,
+}: TabsProps) => {
   return (
     <Link
       href={href}
       className={cn(
-        'h-[44px] md:h-[66px] w-[68px] md:w-[104px] rounded-t-lg bg-grey-900 flex items-center justify-center',
-        isActive ? 'bg-beige-100 border-green border-b-4' : 'group',
+        'h-[44px] md:h-[66px] lg:h-[56px] w-[68px] md:w-[104px] lg:w-[276px] rounded-t-lg lg:rounded-t-none lg:rounded-r-lg bg-grey-900 flex items-center justify-center lg:justify-start lg:pl-8 transition-all duration-300 ease-in-out',
+        isActive
+          ? 'bg-beige-100 border-green border-b-4 lg:border-b-0 lg:border-l-4'
+          : 'group',
+        isCollapsed && 'lg:w-[90px] lg:justify-center lg:pl-0',
       )}
     >
-      <div className="flex flex-col items-center">
+      <div className="flex flex-col items-center lg:flex-row lg:items-center lg:space-x-4">
         <div
           className={cn(
             'w-6 h-6',
@@ -32,10 +43,11 @@ const Tabs = ({ icon, href, label, isActive }: TagsProps) => {
         </div>
         <span
           className={cn(
-            'custom-custom-text-preset-*5-bold hidden md:block',
+            'custom-text-preset-5-bold hidden md:block lg:text-base lg:font-bold whitespace-nowrap overflow-hidden text-ellipsis',
             isActive
               ? 'text-grey-900'
               : 'text-grey-300 group-hover:text-beige-100 transition-all duration-300',
+            isCollapsed && 'lg:hidden',
           )}
         >
           {label}
